ca3aea970927e1c499980015153c00109b91b5ee
Henry Chan <hchan@apache.org>
2014-04-27 20:10:11 -0700

    a

--- src/ca/bcit/comp2613/coursematerial/day03/DinnerTimeException.java
+++ src/ca/bcit/comp2613/coursematerial/day03/DinnerTimeException.java
@@ -1,6 +1,7 @@
 package ca.bcit.comp2613.coursematerial.day03;
 
 // question ... what happens when I change this to Exception instead of RuntimeException ?
+// question ... did I have to change the method signature in the SimpsonsFamily ?
 public class DinnerTimeException extends RuntimeException {
 
 }

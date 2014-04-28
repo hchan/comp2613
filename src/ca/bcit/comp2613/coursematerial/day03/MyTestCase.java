216447d9bb69e33ce820e464fbe5a6ba4b5f520c
Henry Chan <hchan@apache.org>
2014-04-27 21:19:35 -0700

    a

--- src/ca/bcit/comp2613/coursematerial/day03/MyTestCase.java
+++ src/ca/bcit/comp2613/coursematerial/day03/MyTestCase.java
@@ -8,20 +8,28 @@ import org.junit.Test;
 
 public class MyTestCase {
 
+	
+	// question - what happens when I comment out the Annotation above?
 	@Before
 	public void setUp() throws Exception {
 		System.out.println("Hello");
 	}
 
 	@After
+	// question - what happens when I comment out the Annotation above?
 	public void tearDown() throws Exception {
 		System.out.println("Bye");
 	}
 
 	@Test
+	// question - what happens when I comment out the Annotation above?
 	public void test() {
 		System.out.println("In Test");
-		fail("Not yet implemented");
+		String powerful = "Annotations";
+		assertTrue("Annotations".equals(powerful));
 	}
 
+	// fyi ... true story - most new grads start their career as QA developers ...
+	// at least that was what I was told by my first manager
+	// jUnit *could* be your #1 friend
 }

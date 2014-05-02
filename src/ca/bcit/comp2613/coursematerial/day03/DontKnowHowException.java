afef1a083e0e4727ad6be36fc38cba2eb180213c
Henry Chan <hchan@apache.org>
2014-05-01 22:50:50 -0700

    a

--- src/ca/bcit/comp2613/coursematerial/day03/DontKnowHowException.java
+++ src/ca/bcit/comp2613/coursematerial/day03/DontKnowHowException.java
@@ -1,5 +1,6 @@
 package ca.bcit.comp2613.coursematerial.day03;
 
 public class DontKnowHowException extends Exception {
-
+	// question : this is a rather simple Exception class with nothing in its body
+	// how do I generate an Exception with constructors from its parents?
 }

2c5e6e7ea44a5d071dde21e4e8b4553cb4aa80bb
Henry Chan <hchan@apache.org>
2014-05-10 12:53:51 -0700

    a

---
+++ src/ca/bcit/comp2613/coursematerial/day05/FileUtilsDemo.java
@@ -0,0 +1,23 @@
+package ca.bcit.comp2613.coursematerial.day05;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+public class FileUtilsDemo {
+
+	public FileUtilsDemo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		try {
+			FileUtils.writeStringToFile(new File("test.txt"), "Hello World");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}

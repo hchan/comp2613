a02be46ef771be2618719921726c48045f735206
Henry Chan <hchan@apache.org>
2014-05-10 21:53:54 -0700

    c

---
+++ src/ca/bcit/comp2613/coursematerial/day05/Log4jDemo.java
@@ -0,0 +1,16 @@
+package ca.bcit.comp2613.coursematerial.day05;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+public class Log4jDemo {
+
+	public static void main(String[] args) {
+		PropertyConfigurator.configure(
+				Log4jDemo.class.getResourceAsStream("log4j.properties")
+				);
+		Logger log = Logger.getLogger(Log4jDemo.class);
+		log.debug("This is a debug message");
+		log.error("ERROR", new Exception("Doh"));
+	}
+}

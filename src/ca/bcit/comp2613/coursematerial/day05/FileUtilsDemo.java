e2bfb773ababa9a9f2daf6c528ebac365f5b4a2b
Henry Chan <hchan@apache.org>
2014-05-11 19:37:38 -0700

    a

--- src/ca/bcit/comp2613/coursematerial/day05/FileUtilsDemo.java
+++ src/ca/bcit/comp2613/coursematerial/day05/FileUtilsDemo.java
@@ -14,11 +14,27 @@ public class FileUtilsDemo {
 	public static void main(String[] args) {
 		try {
 			FileUtils.writeStringToFile(new File("test.txt"), "Hello World");
-			System.out.println(FileUtils.readFileToString(new File("test.txt")));
+			System.out
+					.println(FileUtils.readFileToString(new File("test.txt")));
+			findTempFileDirectory();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private static void findTempFileDirectory() throws IOException {
+		// create a temp file
+		File temp = File.createTempFile("hello", ".tmp");
+
+		System.out.println("Temp file : " + temp.getAbsolutePath());
+
+		// Get tempropary file path
+		String absolutePath = temp.getAbsolutePath();
+		String tempFilePath = absolutePath.substring(0,
+				absolutePath.lastIndexOf(File.separator));
+
+		System.out.println("Temp file path : " + tempFilePath);
 	}
 
 }
